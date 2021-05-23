2a7391c344e7886d4ddccc3938e64a8e151d7c0d
Muhammad Anza Khan <muhammadanza.khan@ceridian.com>
2021-05-23 14:40:44 -0400

    completed all api methods for controllers

--- WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/Controllers/EmployeeController.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Utilities;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -13,10 +16,56 @@ namespace WebAPI.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private ISqlUtility _sqlUtility;
 
-        public EmployeeController(IConfiguration configuration)
+        public EmployeeController(IConfiguration configuration, ISqlUtility sqlUtility)
         {
             _configuration = configuration;
+            _sqlUtility = sqlUtility;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string sqlQuery = "SELECT EmployeeId, EmployeeName, Department, convert(varchar(10), DateOfJoining, 120) as DateOfJoining, " +
+                "PhotoFileName FROM dbo.Employee";
+            string sqlConnectionString = _configuration.GetConnectionString("EmployeeAppCon");
+            DataTable table = _sqlUtility.SqlClient(sqlConnectionString, sqlQuery);
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Employee employee)
+        {
+            string sqlQuery = $"INSERT INTO dbo.Employee VALUES('{employee.EmployeeName}', '{employee.Department}', '{employee.DateOfJoining}', '{employee.PhotoFileName}')";
+            string sqlConnectionString = _configuration.GetConnectionString("EmployeeAppCon");
+            DataTable table = _sqlUtility.SqlClient(sqlConnectionString, sqlQuery);
+
+            return new JsonResult($"Added new {employee.EmployeeName} to dbo.Employee");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Employee employee)
+        {
+            string sqlQuery = $"UPDATE dbo.Employee SET EmployeeName = '{employee.EmployeeName}', Department = '{employee.Department}', DateOfJoining = '{employee.DateOfJoining}', " +
+                $"PhotoFileName = '{employee.PhotoFileName}' " +
+                $"WHERE EmployeeId = {employee.EmployeeId}";
+
+            string sqlConnectionString = _configuration.GetConnectionString("EmployeeAppCon");
+            DataTable table = _sqlUtility.SqlClient(sqlConnectionString, sqlQuery);
+
+            return new JsonResult($"Updated {employee.EmployeeName} in dbo.Employee");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string sqlQuery = $"DELETE FROM dbo.Employee WHERE EmployeeId={id}";
+            string sqlConnectionString = _configuration.GetConnectionString("EmployeeAppCon");
+            DataTable table = _sqlUtility.SqlClient(sqlConnectionString, sqlQuery);
+
+            return new JsonResult($"Deleted {id} from dbo.Employee");
         }
     }
 }
